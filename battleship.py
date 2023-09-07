8e788242decdaab841f58d584f2df97ae0336124
kinziq <kinzi33@kprschools.com>
2023-09-07 14:17:01 -0400

    starting

---
+++ battleship.py
@@ -0,0 +1,6 @@
+#Name: Kinz
+#Date: 06/09/23
+#Purpose: Battleship...
+#Plan: Game will be in a 5x5 grid. AI will place their ship randomly in that grid with 2 square length ships 5 times.
+#There will be a minimum of 10 incorrect guesses.
+#There will be four array's, one for the player's moves, another for the AI's placement and the last is for counting guesses
