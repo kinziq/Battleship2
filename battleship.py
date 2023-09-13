83129468630d7f0eff11d4fe1a48df98123a7a76
kinziq <kinzi33@kprschools.com>
2023-09-13 14:25:23 -0400

    asd

--- battleship.py
+++ battleship.py
@@ -3,25 +3,29 @@
 #Purpose: Battleship
 #Plan: Game will be in a 5x5 grid. AI will place their ship randomly in that grid with 2 square length ships 5 times.
 #There will be a minimum of 10 incorrect guesses.
-#There will be four array's, one for the player's moves, another for the AI's placement and the last is for counting guesses
+import os
 import random
 from time import sleep
 
+os.system("cls")
+
+global board='ooooooooooooooooooooooooo'
 (input("Welcome to Battleship. Please type in any character on your keyboard when you are ready to start: "))
 
-rulesmessage =("This game will be seperated into a 5x5 grid. The AI will random place their own ships as this will be a one way. You will get 10 incorrect tries after which the game will end. The board will now generate soon.")
+rulesmessage =("This game will be seperated into a 5o5 grid. The AI will random place their own ships as this will be a one way. You will get 10 incorrect tries after which the game will end. The board will now generate soon.")
 print(f"{rulesmessage}")
 sleep(4)
 
-grid  = [" | 1 | 2 | 3 | 4 | 5 |"]
-gridA = ["A| x | x | x | x | x |"]
-gridB = ["B| x | x | x | x | x |"]
-gridC = ["C| x | x | x | x | x |"]
-gridD = ["D| x | x | x | x | x |"]
-gridE = ["E| x | x | x | x | x |"]
-print(f"{grid}")
-print(f"{gridA}")
-print(f"{gridB}")
-print(f"{gridC}")
-print(f"{gridD}")
-print(f"{gridE}")
+#board
+print("  A B C D E")
+print( f'1 {board[0]} {board[1]} {board[2]} {board[3]} {board[4]}')
+print( f'2 {board[5]} {board[6]} {board[7]} {board[8]} {board[9]}')
+print( f'3 {board[10]} {board[11]} {board[12]} {board[13]} {board[14]}')
+print( f'4 {board[15]} {board[16]} {board[17]} {board[18]} {board[19]}')
+print( f'5 {board[20]} {board[21]} {board[22]} {board[23]} {board[24]}')
+
+guess = input("Please input your guess (A2, 2B, D3 etc.): ")
+
+if guess == ("2B"):
+  	{board[6]} == ("x")
+print({board[6]})
