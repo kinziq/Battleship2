593a09968cd307b7a694cbd8df0d8d696c8977d9
kinziq <kinzi33@kprschools.com>
2023-09-19 14:25:29 -0400

    almost

--- battleship.py
+++ battleship.py
@@ -19,14 +19,15 @@ print(f"{rulesmessage}")
 
 
 #board
-print("  A B C D E")
-print( f'1 {board[0]} {board[1]} {board[2]} {board[3]} {board[4]}')
-print( f'2 {board[5]} {board[6]} {board[7]} {board[8]} {board[9]}')
-print( f'3 {board[10]} {board[11]} {board[12]} {board[13]} {board[14]}')
-print( f'4 {board[15]} {board[16]} {board[17]} {board[18]} {board[19]}')
-print( f'5 {board[20]} {board[21]} {board[22]} {board[23]} {board[24]}')
+def boardlayout():
+  print("  A B C D E")
+  print( f'1 {board[0]} {board[1]} {board[2]} {board[3]} {board[4]}')
+  print( f'2 {board[5]} {board[6]} {board[7]} {board[8]} {board[9]}')
+  print( f'3 {board[10]} {board[11]} {board[12]} {board[13]} {board[14]}')
+  print( f'4 {board[15]} {board[16]} {board[17]} {board[18]} {board[19]}')
+  print( f'5 {board[20]} {board[21]} {board[22]} {board[23]} {board[24]}')
 
-
+boardlayout()
 turns = 0
 leftturns = 10
 shipguess = dict({"A":0, "B":1, "C":2, "D":3, "E":4}) 
@@ -38,14 +39,12 @@ while run:
     #Will add a turn counter if the length of the characters are equal to two to the variable turns
     turns+=1
     leftturns-=1
-    
     letter = guess[0]
     row = int(guess[1])
     spot = shipguess[letter] + (row-1)*5
   
   board[spot] = ('x')
-  
-  print (f'{board}')
+  boardlayout()
   print(f"You have guessed {turns} times. You have {leftturns} turns left")
   if turns == 10:
     run = False
